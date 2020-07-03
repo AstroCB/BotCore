0e2f5999f29c35a78ed44afdcc170e006ae0a4e5
Cameron Bernhardt <cambernhardt@me.com>
2020-07-03 01:07:53 -0400

    Add example credentials script

---
+++ examples/set_credentials.sh
@@ -0,0 +1,10 @@
+# This is an example script you can run to set up your environment variables to
+# be used as a credentials object (see docs) for logging in to BotCore. Change
+# the values to your actual information and add these lines to your
+# .bashrc/.zshrc or equivalent.
+
+export MEMCACHIER_USERNAME="1234A"
+export MEMCACHIER_PASSWORD="ABCDEFGHIJKLMNOPQRSTUVWXYZ12345"
+export MEMCACHIER_SERVERS="mc1.dev.ec2.memcachier.com:00000"
+export FACEBOOK_EMAIL="email@example.com"
+export FACEBOOK_PASSWORD="example_password"
